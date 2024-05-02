11b9ff35a494723f08b77f955ab6a04f147257b6
Ekaterina-Titareva <ekaterina.titareva.ca@gmail.com>
2024-05-02 23:50:10 +0300

    style, form

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,13 +6,16 @@ function Header() {
 return (
     <header className={styles.header}>
     <div className={styles.logo}>
-        <h2>Expand your vocabulary with</h2>
-        <div className={styles.imgWrapper}>
-            <img  className={styles.img} src={logo} alt='logo'/>
+        <h2 className={styles.logoText}>Expand your vocabulary with</h2>
+        <div className={styles.logoI}>
+            <div className={styles.imgWrapper}>
+                <img  className={styles.img} src={logo} alt='logo'/>
+            </div>
+            <h1 className={styles.logoName}>LexiCa</h1> 
         </div>
-        <h1 className={styles.logoName}>LexiCa</h1> 
-        <h2>where words come to life!</h2>
+        <h2 className={styles.logoText}>where words come to life!</h2>
     </div>
+    
     <nav>
         <ul>
             <li><Link className={styles.link} to={'/LexiCa/'}>Home</Link></li>
