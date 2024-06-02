a683d028ec10225b1081bb9a04f71a9de9e47972
Ekaterina-Titareva <ekaterina.titareva.ca@gmail.com>
2024-06-02 21:41:10 +0300

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,9 @@
+import styles from "./footer.module.css"
 
 function Footer() {
     return (
-        <footer>
+        <footer className={styles.footer}>
+            <p>Если Вы столкнулись с проблемой на сайте, напишите, пожалуйста, на почту: <a className={styles.link} href="mailto:ekaterina.titareva.ca@gmail.com">ekaterina.titareva.ca@gmail.com</a> </p>
         </footer>
     )
 }
